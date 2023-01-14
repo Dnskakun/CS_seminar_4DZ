80d0996eebc698c301634cfcdb07725ff36bb1ae
dnskakun <dimon_sdn@list.ru>
2023-01-14 17:53:18 +0300

    Добавлена задача №2

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Программа, которая принимает на вход число и
+//выдает сумму цифр в числе.
+
+int num = GetNumberFromUser("Введите целое число", "Ошибка ввода!");
+int res = GetResult(num);
+
+Console.WriteLine($"{num} -> {res}");
+
+
+//Функции, используемые в программе
+int GetNumberFromUser(string message, string errormMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+            Console.WriteLine(errormMessage);
+    }
+}
+
+int GetResult(int number)
+{
+    int result = 0;
+    int absNumber = Math.Abs(number);
+    while (absNumber > 0)
+    {
+        result = result + absNumber%10;
+        absNumber = absNumber/10;
+    }
+    return result;
+}

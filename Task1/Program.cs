040980a4c02efd1981cca586b43c3515cc885937
dnskakun <dimon_sdn@list.ru>
2023-01-14 17:25:16 +0300

    Добавлена задача №1

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите цикл, который принимает на вход два числа и
+//возводит первое число в натуральную степень второго числа.
+
+int a = GetNumberFromUser("Введите целое число А", "Ошибка ввода!");
+int b = GetNumberFromUser("Введите целое число В", "Ошибка ввода!");
+int expResult = GetResult(a, b);
+
+Console.WriteLine($"{a},{b} -> {expResult}");
+
+
+//Функции, используемые в программе
+int GetNumberFromUser(string message, string errormMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+            Console.WriteLine(errormMessage);
+    }
+}
+
+int GetResult(int numA, int numB)
+{
+    int result = numA;
+    for (int i = 1; i < numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}

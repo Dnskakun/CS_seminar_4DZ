18fc923b5e6d75891b385994cd3421c4ad28bdd3
dnskakun <dimon_sdn@list.ru>
2023-01-15 12:43:38 +0300

    Добавлена задача №3

---
+++ Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿//Программа, которая задает массив из 8 элементов и
+//выводит их на экран
+
+int[] array = new int[8];
+
+GetFillArrayFromUser(array);
+GetPrintArrayFromUser(array);
+
+//Функции, используемые в программе
+void GetFillArrayFromUser(int[] collection)
+{
+    int lengthArray = collection.Length;
+    for (int i = 0; i < lengthArray; i++)
+    {
+        while (true)
+        {
+            Console.Write($"Введите {i}-й элемент массива: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+            if (isCorrect)
+                collection[i] = num;
+            else Console.WriteLine("Ошибка ввода!");
+            break;
+        }
+    }
+}
+
+void GetPrintArrayFromUser(int [] printArray)
+{
+    int lengthArray = printArray.Length;
+    Console.Write("[");
+    for (int i = 0; i < lengthArray; i++)
+    {
+        Console.Write(printArray[i]);
+        if (i < lengthArray - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
